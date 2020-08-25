92f38f9fa82f1880a5ba5efd9c0b1bfb0fe6583e
Steven Doran <steven.doran@flatironschool.com>
2020-08-25 13:14:15 -0400

    post lecture

--- 21-intro-to-events/index.js
+++ 21-intro-to-events/index.js
@@ -1,5 +1,48 @@
 console.log("Events.")
 
-// Let's add the best movie ever made to our list...
+document.addEventListener("DOMContentLoaded", function(e){ 
+
+  // Let's add the best movie ever made to our list...
 
 
+  // √1. create a new li element
+  // √2. give the li a class of "movie"
+  // √3. add all that internal HTML to the li with specific Goonies data
+  // √4. add it to the DOM somehow
+    // get the ul from the DOM
+    // add the li to the bottom of the ul
+
+
+  // this is standing in for some external datasource
+  const gooniesObj = {
+    title: "The Goonies",
+    year: 1985,
+    score: 10,
+    imageUrl: "https://cdn.shopify.com/s/files/1/1416/8662/products/goonies_1985__linen_original_film_art_f_1200x.jpg?v=1586639573",
+    genre: "Action"
+  }
+
+  function renderMovie(movieObj){
+    const movieLi = document.createElement("li")
+    // movieLi.className = "movie"
+    movieLi.classList.add("movie")
+    
+    movieLi.innerHTML = `
+      <h3>${movieObj.title}</h3>
+      <img alt=""
+      src="${movieObj.imageUrl}" />
+      <h4>Year: ${movieObj.year}</h4>
+      <h4>Genre: ${movieObj.genre}</h4>
+      <h4>Score: <span>${movieObj.score}</span> </h4>
+      <button class="up-vote">Up Vote</button>
+      <button>Down Vote</button>
+      <button>&times;</button>
+    `
+    
+    const movieList = document.querySelector('#movie-list')
+    movieList.append(movieLi)
+  }
+
+  renderMovie(gooniesObj)
+
+})
--- 21-intro-to-events/listeners.js
+++ 21-intro-to-events/listeners.js
@@ -2,6 +2,16 @@ console.log("listening")
 
 // As a user, when I click on the welcome image I want to see a turquoise border around that image
 
+// √1. get the image from the DOM
+// 2. let's add a click listener to the image
+// 3. on click, change it's border somehow...
+
+const welcomeImage = document.getElementById("welcome-image")
+
+welcomeImage.addEventListener("click", function(e){
+  const image = e.target
+  image.style.border = "solid thick turquoise"
+})
 
 // As a user, when the mouse moves over the welcome image I want to see a picture of a baby sloth, and I want the picture to change back when the mouse moves off the image
 
