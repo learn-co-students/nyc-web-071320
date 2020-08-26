d9b314972b490ef3ea94f3caf3e258bd202458f6
Steven Doran <steven.doran@flatironschool.com>
2020-08-26 11:41:22 -0400

    post lecture

--- 23-event-delegation/index.js
+++ 23-event-delegation/index.js
@@ -2,95 +2,18 @@ console.log("More events")
 
 document.addEventListener("DOMContentLoaded", function(e){ 
 
+  const movieList = document.querySelector('#movie-list')
+
   // this is standing in for some external datasource
-  const gooniesObj = {
-    title: "The Goonies",
-    year: 1985,
-    score: 10,
-    imageUrl: "https://cdn.shopify.com/s/files/1/1416/8662/products/goonies_1985__linen_original_film_art_f_1200x.jpg?v=1586639573",
-    genre: "Action"
-  }
+  // const gooniesObj = {
+  //   title: "The Goonies",
+  //   year: 1985,
+  //   score: 10,
+  //   imageUrl: "https://cdn.shopify.com/s/files/1/1416/8662/products/goonies_1985__linen_original_film_art_f_1200x.jpg?v=1586639573",
+  //   genre: "Action"
+  // }
 
-  function renderMovie(movieObj){
-    const movieLi = document.createElement("li")
-    movieLi.classList.add("movie")
-    
-    movieLi.innerHTML = `
-      <h3>${movieObj.title}</h3>
-      <img alt=""
-      src="${movieObj.imageUrl}" />
-      <h4>Year: ${movieObj.year}</h4>
-      <h4>Genre: ${movieObj.genre}</h4>
-      <h4>Score: <span>${movieObj.score}</span> </h4>
-      <button class="up-vote">Up Vote</button>
-      <button>Down Vote</button>
-      <button>&times;</button>
-    `
-    
-    const movieList = document.querySelector('#movie-list')
-    movieList.append(movieLi)
-  }
-
-  renderMovie(gooniesObj)
-
-})
-
-/*
-
-1. As a user, when I click the "Up Vote" button for the Jaws movie, I should see the score for the Jaws movie increment by 1.
-
-
-2. As a user, when I click "Up Vote" for any given movie, the score for that movie should increment by 1.
-
-
-3. As a user I should see all the movies in the movie collection displayed on the DOM
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // kinda like a db/external data source for the moment
 const movieCollection = [
   {
     title: 'The Goonies',
@@ -149,3 +72,93 @@ const movieCollection = [
     imageUrl: "https://resizing.flixster.com/h8e7W7cVaQhuLdSvABDkJk6r5sc=/206x305/v1.bTsxMTE2NjE5OTtqOzE4MzU0OzEyMDA7ODAwOzEyMDA"
   },
 ]
+
+  // function renderMovies(movies){} // => equivalent to the below function expression
+  
+  const renderMovies = movies => {
+    // iterate through the movie collection
+    // render each movie to the DOM
+
+    for(const movieObj of movies){
+      renderMovie(movieObj)
+    }
+  }
+
+  function renderMovie(movieObj){
+    const movieLi = document.createElement("li")
+    movieLi.classList.add("movie")
+    
+    movieLi.innerHTML = `
+      <h3>${movieObj.title}</h3>
+      <img alt=""
+      src="${movieObj.imageUrl}" />
+      <h4>Year: ${movieObj.year}</h4>
+      <h4>Genre: ${movieObj.genre}</h4>
+      <h4>Score: <span>${movieObj.score}</span> </h4>
+      <button class="up-vote">Up Vote</button>
+      <button>Down Vote</button>
+      <button data-purpose='delete'>&times;</button>
+    `
+    
+    movieList.append(movieLi)
+  }
+
+  // const jawsUpVote = document.querySelector('.up-vote')
+
+
+  
+  // const upVoteButtons = document.querySelectorAll('.up-vote')
+  
+  // for(const upVoteButton of upVoteButtons){
+  //   upVoteButton.addEventListener('click', function(e){
+      // const button = e.target
+      // // const span = button.previousElementSibling.children[0]
+      // // const span = button.parentElement.querySelector('span')
+      // const span = button.closest('.movie').querySelector('span')
+      // const currentScore = parseInt(span.textContent)
+      // const newScore = currentScore + 1
+      // span.textContent = newScore
+  //   })
+  // }
+
+
+  function clickHandler() {
+    movieList.addEventListener('click', function(e){
+      if(e.target.matches(".up-vote")){
+        const button = e.target
+        // const span = button.previousElementSibling.children[0]
+        // const span = button.parentElement.querySelector('span')
+        const span = button.closest('.movie').querySelector('span')
+        const currentScore = parseInt(span.textContent)
+        const newScore = currentScore + 1
+        span.textContent = newScore
+      } else if(e.target.matches('[data-purpose="delete"]')){
+        e.target.parentElement.remove()
+      }
+    }) 
+  }
+
+  clickHandler()
+  // renderMovie(gooniesObj)
+  renderMovies(movieCollection)
+})
+
+/*
+
+1. As a user, when I click the "Up Vote" button for the Jaws movie, I should see the score for the Jaws movie increment by 1.
+
+  * √get Jaws up-vote button from the DOM
+  * √add a click listener to it
+  * √create a callback function that will increment the score by one point
+
+2. As a user, when I click "Up Vote" for any given movie, the score for that movie should increment by 1.
+
+  √* get all the up vote buttons
+  √* iterate through them and add a click listener to each one that does the increment stuff
+
+
+3. As a user I should see all the movies in the movie collection displayed on the DOM
+
+
+*/
+
