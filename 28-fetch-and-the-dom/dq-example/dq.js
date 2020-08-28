a7d0db00189a2adb1c1f928cfe398507d02d4e69
Steven Doran <steven.doran@flatironschool.com>
2020-08-28 10:27:35 -0400

    pre lecture again

--- 28-fetch-and-the-dom/dq-example/dq.js
+++ 28-fetch-and-the-dom/dq-example/dq.js
@@ -9,36 +9,3 @@ Open up the Github repository for this reading and view the provided `src/index.
 
 
 */
-
-document.addEventListener("DOMContentLoaded", e => {
-  const button = document.querySelector("button")
-
-  const clickHandler = () => {
-    button.addEventListener("click", e => {
-      fetch("https://randomuser.me/api/")
-      .then(res => res.json())
-      .then(data => {
-        const user = data.results[0]
-        renderUser(user)
-      })
-    })
-  }
-
-  const renderUser = user => {
-    console.log(user)
-
-    const picture = user.picture.large
-    document.querySelector('#profile_picture').src = picture
-    
-    const first = user.name.first
-    const last = user.name.last
-    const title = user.name.title
-    document.querySelector('#fullname').textContent = `${title} ${first} ${last}`
-    
-    const email = user.email
-    document.querySelector('#email').textContent = email
-  }
-
-  clickHandler()
-})
-
