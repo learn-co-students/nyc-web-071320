aca6cbbc50b9e4a6e308fbd560bc56ef661bab71
Steven Doran <steven.doran@flatironschool.com>
2020-09-08 12:50:57 -0400

    post lecture

--- 34-formalizing-js/index.js
+++ 34-formalizing-js/index.js
@@ -34,18 +34,43 @@ the inner function have access to the variables and other resources of their par
   // a. Global Scope
   // variables declared in the global scope are visible to any nested function in the same scope
 
+  // const name = "Steven"
 
+  // const sayHi = () => {
+    // console.log(`Hi there ${name}`)
+  // }
 
 
   // b. Function Scope
   // variables declared in function scope are visible within the function and any nested functions inside of it, but not visible from outside of the function
   
+  // let name = "Michael"
 
+  // const sayHi = () => {
+  //   let firstName = "Steven"
+  //   let something = () =>  {
+  //     console.log("something", firstName)
+  //   }
+  //   // console.log(`Hi there ${firstName}`)
+  //   something()
+  // }
+
+  // console.log(firstName)
 
   // c. Block Scope
   // variables declared in block scope are visible within the block and any nested functions inside of it, but not visible from outside of the block
  
+  // if(true){
+  //   const firstName = "Steven"
 
+  //   let something = () =>  {
+  //     console.log("something", firstName)
+  //   }
+
+  //   something()
+  //  }
+
+  //  console.log(firstName)
   
 // ---------------------------------------------------------------------------------------------------------------------------------
 // 2. HOISTING
@@ -55,10 +80,30 @@ when JS is being compiled, certain functions and variables are "hoisted" to the 
 
 // variables declared with `var` are hoisted, `let` and `const` are not
 
+// console.log("first", dog)
+
+// var dog = "Perky"
+
+// console.log("second", dog)
+
 
 // function declarations are hoisted, arrow functions and function expressions are not
 
+// bark()
 
+// function bark(){
+//   console.log("Woof")
+// }
+
+// does not get hoisted
+// const bark = function(){
+//   console.log("Woof expression")
+// }
+
+// does not get hoisted
+// const bark = () => {
+//   console.log("Woof arrow")
+// }
 
 // ---------------------------------------------------------------------------------------------------------------------------------
 // 3. VARIABLE DECLARATION
@@ -67,19 +112,48 @@ when JS is being compiled, certain functions and variables are "hoisted" to the 
   // variables declared with var can be both redeclared and redefined
 
 
+  // var dog = "Perky"
+  // var dog = "Neikko"
 
 
   // b. let
   // let allows us to redefine a variable, but not redeclare it
 
+  // // this will error out
+  // let cat = "Houdini"
+  // let cat = "Edgar"
 
-
+  // // this is cool
+  // let cat = "Houdini"
+  // cat = "Edgar"
 
 
   // c. const
   // const does not allow us to either redeclare or redefine a variable
 
 
+  //  // this will error
+  //  const horse = "Benny"
+  //  const horse = "Lady"
+
+  // // this will error
+  // const horse = "Benny"
+  // horse = "Lady"
+
+  // this is cool
+  // const fish = {
+  //   name: "Chris",
+  //   species: "Goldfish"
+  // }
+
+  // fish.name = "Christopher"
+  // fish.age = 12
+
+  // // this is not cool
+  // fish = {
+  //   name: "Chris",
+  //   species: "Goldfish"
+  // }
 
 
   
@@ -96,18 +170,37 @@ when JS is being compiled, certain functions and variables are "hoisted" to the 
   // a. callbacks 
   // when functions are passed as arguments to other functions
 
+  const myMap = (array, callback) => {
+    const newArray = []
+
+    for(let item of array){
+      const newItem = callback(item)
+      newArray.push(newItem)
+    }
+    
+    return newArray
+  }
 
   
   // b. higher order functions
   // functions that accept functions as parameters or that return other functions
 
-
+  const multiply = num1 => {
+    return num2 => {
+      return num1 * num2
+    }
+  }
+  
 
   // c. closures
   // a closure is when a function is able to remember and access its
   // scope even when that function is executing outside of its original scope
   // a function with a backpack, a function with baggage
 
+  // carries around the num1 variable in its backpack and uses it
+  // at the time of invokation even though it is no longer "in scope"
+  const doubler = multiply(2)
+  
 
 
 
